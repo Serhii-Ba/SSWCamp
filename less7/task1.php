1cec0d65887dc504cd07c73a0c865a0009957c5b
Serhii-Ba <tatko.tatko8@gmail.com>
2022-05-03 19:02:59 +0300

    add 1 tasks from lesson #7

--- less7/task1.php
+++ less7/task1.php
@@ -1,46 +1,77 @@
 <?php
-class Animals {
+class Animal {
 
     public $food;
     public $location;
     public function makeNoise() {
-        echo "Така тварина спить";
+        echo "така тварина спить";
     }
     public function eat() {
-
+        echo "така тварина їсть";
     }
     public function sleep() {
-
+        echo "така тварина спить";
     }
 }
 
 class Cat extends Animal {
 
-    public $name = 'Кішка';
+    public $food = "рибу";
+    public $name = 'кішка';
     public function makeNoise() {
         echo "Мяу-мяу";
     }
     public function eat() {
-
+        print_r($this->food);
+    }
+    public function sleep() {
+        print_r($this->name + "тварина спить");
     }
 }
 
 class Dog extends Animal {
 
-    public $name = 'Собака';
-    public function makeNois() {
+    public $food = "м`ясо";
+    public $name = "собака";
+    public function makeNoise() {
         echo "Гав-гав";
     }
     public function eat() {
-
+        print_r($this->food);
+    }
+    public function sleep() {
+        print_r($this->name + "тварина спить");
     }
 }
 
 class Horse extends Animal {
 
-    public $name = 'Кінь';
+    public $food = "сіно";
+    public $name = 'кінь';
+    public function makeNoise() {
+        echo "Іго-го";
+    }
+    public function eat() {
+        print_r($this->food);
+    }
+    public function sleep() {
+        print_r($this->name + "тварина спить");
+    }
 }
 
 class Veterinarian {
 
+    public function treatAnimal (Animal $animal) {
+    $animal->makeNoise();
+    $animal->eat();
+    }
+}
+
+$animals = [];
+$animals[0] = new Cat();
+$animals[1] = new Dog();
+$animals[2] = new Horse();
+
+for ($i=0;$i<3;$i=$i+1){
+    $v = $
 }
